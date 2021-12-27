0fdcb8df16bfc04be27da67787df95fec3192a62
Lukasz Blachnicki <lblachnicki@gmail.com>
2021-12-27 12:42:24 +0100

    Set up mdx pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withMDX({
   reactStrictMode: true,
-}
+  pageExtensions: ["tsx", "js", "jsx", "md", "mdx"],
+});
