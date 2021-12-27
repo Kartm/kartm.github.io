9904bf89ad0187139d15998f3ad39e4f0ac5f66a
Lukasz Blachnicki <lblachnicki@gmail.com>
2021-12-27 14:04:01 +0100

    Add GTM support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
+import { TrackingHeadScript } from "@phntms/next-gtm";
+
+const REACT_APP_GA_TRACKING_ID = process.env.REACT_APP_GA_TRACKING_ID || "";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <TrackingHeadScript id={REACT_APP_GA_TRACKING_ID} />
       <Component {...pageProps} />
     </Layout>
   );
