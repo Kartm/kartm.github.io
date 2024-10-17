8c0faba3488bc9df0ee7db8cd0bdfa3b7578e4d7
Łukasz Blachnicki <lblachnicki@gmail.com>
2024-10-17 16:33:54 +0200

    Add link to survey

--- pages/projects/masters-thesis.tsx
+++ pages/projects/masters-thesis.tsx
@@ -15,6 +15,10 @@ const Home: NextPage = () => {
       <main className={styles.main}>
           <section>
               <h2 style={{textAlign: 'center'}}>{`Master's thesis - Tools and practices to improve developer efficiency`}</h2>
+              <p style={{textAlign: 'center', marginTop: '16px'}}>
+                  If you’re a software developer, click here to take part in <a href="https://docs.google.com/forms/d/e/1FAIpQLScRz9xsQakCssFqv_Jew1diBdGDr1POvcbRRpIubTnKSzPzYQ/viewform?usp=sf_link">my
+                  survey</a>.
+              </p>
               <div style={{display: 'flex', justifyContent: 'center'}}>
                   <Image
                       src="/gifs/AtAthens6321underconstruction_lemmings.gif"
@@ -31,16 +35,11 @@ const Home: NextPage = () => {
                       unoptimized
                   />
               </div>
-              <iframe
-                  style={{marginTop: '32px'}}
-                  src="https://docs.google.com/forms/d/e/1FAIpQLSeqr9wSBcmEX3Q202Ha5vnX8n8rccDfuS7ohr4Uej_Nt2IMdQ/viewform?embedded=true"
-                  width="100%"
-                  height="600"
-                  frameBorder="0"
-                  marginHeight={0}
-                  marginWidth={0}
-              >Loading…
-              </iframe>
+              <p style={{textAlign: 'center', marginTop: '16px'}}>
+                  Discover the key findings from the survey!
+                  Submit <a href="https://docs.google.com/forms/d/e/1FAIpQLSeqr9wSBcmEX3Q202Ha5vnX8n8rccDfuS7ohr4Uej_Nt2IMdQ/viewform?usp=sf_link">this
+                  form</a> to receive the summary as soon as it’s ready.
+              </p>
           </section>
       </main>
     </div>
