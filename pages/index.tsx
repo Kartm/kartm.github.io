7ff9f8b265896edae76bd0ead1d8d4e43adcd40d
Łukasz Blachnicki <lblachnicki@gmail.com>
2025-10-04 10:29:37 +0200

    Redirect to blogpost

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,18 @@
 import styles from "../styles/Home.module.css";
-import Image from "next/image";
 import type { NextPageWithLayout } from './_app'
 import MainLayout from "../layouts/main-layout";
+import {useEffect} from "react";
 
 const Home: NextPageWithLayout = () => {
+    useEffect(() => {
+        const url = "/projects/what-really-matters-in-software-developer-productivity"
+        window.location.href = url
+    })
+
     return (
         <div className={styles.container}>
             <main className={styles.main}>
-                <section>
-                    <div>
-                        <Image
-                            src="/gifs/john-travolta-lost.gif"
-                            width="220"
-                            height="212"
-                            unoptimized
-                            alt="placeholder"
-                        />
-                    </div>
-                </section>
+                <section/>
             </main>
         </div>
     );
